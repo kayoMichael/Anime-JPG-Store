eae7c03110a18df83520ba01ef609736a71d524c
kayoMichael <michaelxkayo@hotmail.com>
2024-08-12 18:15:52 -0400

    feat: navbar

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,15 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.purple,
+        accent: colors.purple[600],
+        'accent-foreground': colors.white,
+      },
+    },
   },
   plugins: [],
 };
